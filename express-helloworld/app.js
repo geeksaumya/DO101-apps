13ad86d238c8dac10f424a1d9ad7b749f56023c2
Saumya Kale <saumyakale29@gmail.com>
2025-01-25 08:16:00 +0530

    add /mars endpoint

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -1,14 +1,15 @@
 var express = require('express');
 app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!\n');
+ap5p.get('/', function (req, res) {
+  var response = 'This is version 2 of the app.' + '\n';  
+  res.send(response);
 });
 
 app.get('/mars', function(req, res) {
   res.send('Hello Mars!\n');
 });
-
+// This is a new route that takes a parameter, called name.
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
